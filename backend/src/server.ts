0ad6cb5e2a32473fd6efac26fe1ff49bbd70bdc9
kdds22 <klaus.dd.sa@gmail.com>
2020-10-13 22:52:51 -0300

    Implementing Routes concept and starting backend

---
+++ backend/src/server.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/users', (request, response) => {
+    return response.json({ "message": "Hello World" });
+});
+
+app.post('/users', (request, response) => {
+    console.log(request.query);
+    console.log(request.params);
+    
+    return response.json({ "message": "Hello World" });
+});
+
+app.post('/users/:id', (request, response) => {
+    console.log(request.query);
+    console.log(request.params);
+    console.log(request.body);
+    
+    return response.json({ "message": "Hello World" });
+});
+
+
+app.listen(3333);
+
+// QUery: http://localhost:3333/users
+// Route Params: http://localhost:3333/users/1
+// Body: http://localhost:3333/users
+// QUery Params: http://localhost:3333/users?search=klaus
