ae90fed4c460e4a17c21383ce65faf7501f0bdb7
kdds22 <klaus.dd.sa@gmail.com>
2020-10-14 00:32:14 -0300

    Fix routes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,7 +4,7 @@ import OrphanagesController from './controllers/OrphanagesController';
 const routes = Router();
 
 routes.post('/orphanages', OrphanagesController.create);
-routes.get('/orphanages', OrphanagesController.index);
-routes.get('/orphanages/:id', OrphanagesController.show);
+routes.get('/orphanages', OrphanagesController.show);
+routes.get('/orphanages/:id', OrphanagesController.index);
 
 export default routes;
