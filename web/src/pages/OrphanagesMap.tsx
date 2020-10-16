798e0b2446aa16134144a81b7caec81226afcd30
kdds22 <klaus.dd.sa@gmail.com>
2020-10-16 00:11:52 -0300

    Fixing Dependencies and Implement map marker concept

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import Leafleat from 'leaflet';
 
 import 'leaflet/dist/leaflet.css';
 
-import { Map, TileLayer } from 'react-leaflet';
 
 import mapMarkerImg from '../images/map-marker.svg';
 
 
 import '../styles/pages/orphanages-map.css';
 
+const mapIcon = Leafleat.icon({
+    iconUrl: mapMarkerImg,
+    
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, -5]
+});
 
 function OrphanagesMap() {
     return (
@@ -20,7 +28,7 @@ function OrphanagesMap() {
                     <img src={mapMarkerImg} alt="Happy" />
 
                     <h2>Escolha um orfanato no mapa</h2>
-                    <p>Muitas crianças estão esperando a sua visita :)</p>
+                    <p>Muitas crianças estão esperando a sua visita :D </p>
 
                 </header>
                 <footer>
@@ -35,10 +43,19 @@ function OrphanagesMap() {
                 style={{ width: '100%', height: '100%' }}
             >
                 {/*<TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"  />*/}
-                <TileLayer 
+                <TileLayer
                     url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
                 />
+
+                <Marker icon={mapIcon} position={[-10.9389873, -37.0677812]}>
+                    <Popup closeButton={false} minWidth={240} maxWidth={240}>
+                        Algum Orfanato de Aracaju
+                    </Popup>
+                </Marker>
             </Map>
+
+
+
 
             <Link to="" className="create-orphanage">
                 <FiPlus size={32} color="#FFF" />
